c21a063b2ef6958247430a656eb2646657984799
Eurico Moutinho <yungsniper@hotmail>
2022-10-04 21:54:27 +0100

    e-commerce

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -3,15 +3,23 @@ import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import {LinkContainer} from 'react-router-bootstrap'
 import { NavLink } from 'react-router-dom'
-import { Badge, Nav } from 'react-bootstrap'
+import { Badge, Nav, NavDropdown } from 'react-bootstrap'
 import { Store } from '../Store'
+import {ToastContainer} from 'react-toastify';
 
 
 function Header() {
-  const {state} = useContext(Store);
-  const {cart} =state;
+  const {state, dispatch: ctxDispatch} = useContext(Store);
+  const {cart, userInfo} =state;
+
+  const signoutHandler = () => {
+    ctxDispatch({type: 'USER_SIGNOUT'});
+    localStorage.removeItem('userInfo');
+  }
+
   return (
-    <div>
+    <div className='d-flex flex-column site-container'>
+      <ToastContainer position='bottom-center' limit={1}/>
         <header>
           <Navbar bg='dark' variant='dark' >
             <Container>
@@ -26,6 +34,24 @@ function Header() {
                   </Badge>
                 )}
               </NavLink>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id='basic-nav-dropdown'>
+                  <LinkContainer to='/profile'>
+                      User Profile
+                  </LinkContainer>
+                  <LinkContainer to='/orderhistory'>
+                      Order History
+                  </LinkContainer>
+                  <NavDropdown.Divider />
+                  <NavLink className="dropdown-item" to='#signout' onClick={signoutHandler}>
+                  Sign Out
+                </NavLink>
+                </NavDropdown>
+              ):(
+                <NavLink className="nav-link" to='/signin'>
+                  Sign In
+                </NavLink>
+              )}
             </Nav>
             </Container>
           </Navbar>
--- src/pages/SigninScreen.jsx
+++ src/pages/SigninScreen.jsx
@@ -1,13 +1,41 @@
-import React from 'react'
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import React, { useContext, useEffect, useState } from 'react'
 import { Button, Container, Form } from 'react-bootstrap'
 import { Helmet } from 'react-helmet-async'
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+import { Store } from '../Store';
+import { getError } from '../utils/utils';
 
 
 function SigninScreen() {
+    const navigate = useNavigate();
     const {search} = useLocation();
     const redirectIntUrl = new URLSearchParams(search).get('redirect');
     const redirect = redirectIntUrl ? redirectIntUrl : '/';
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const {state, dispatch: ctxDispatch} = useContext(Store);
+    const {userInfo} = state;
+    const submitHandler = (e) => {
+        e.preventDefault();
+        axios
+        .post('http://localhost:5005/api/users/signin', {
+            email,
+            password,
+        }).then((data)=> {ctxDispatch({type: 'USER_SIGNIN', payload: data})
+        localStorage.setItem('userInfo', JSON.stringify(data))
+    navigate(redirect || '/')}
+        ).catch((err) => toast.error(getError(err)))
+    }
+
+    useEffect(()=>{
+        if(userInfo){
+            navigate(redirect);
+        }
+    }, [navigate, redirect, userInfo])
 
   return (
     <Container className="small-container">
@@ -15,14 +43,14 @@ function SigninScreen() {
             <title>Sign In</title>
         </Helmet>
         <h1 className="my-3">Sign In</h1>
-        <Form>
+        <Form onSubmit={submitHandler}>
             <Form.Group className='mb-3' controlId='email'>
                 <Form.Label>Email</Form.Label>
-                <Form.Control type='email' required />
+                <Form.Control type='email' required onChange={(e)=> setEmail(e.target.value)} />
             </Form.Group>
             <Form.Group className='mb-3' controlId='password'>
                 <Form.Label>password</Form.Label>
-                <Form.Control type='password' required />
+                <Form.Control type='password' required onChange={(e)=> setPassword(e.target.value)}/>
             </Form.Group>
             <div className='mb-3'>
                 <Button type='submit'>Sign In</Button>
